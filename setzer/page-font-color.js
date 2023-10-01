257300d39699d18c574496fd6b9c462320eb57d1
kinten108101 <kinten108101@protonmail.com>
2023-10-02 00:09:01 +0700

    setzer: font color functional dialog

--- setzer/page-font-color.js
+++ setzer/page-font-color.js
@@ -1,4 +1,7 @@
 import GObject from "gi://GObject";
+import Gtk from "gi://Gtk";
+
+const window = workbench.builder.get_object("window");
 
 const expander = workbench.builder.get_object("expander");
 const sw = workbench.builder.get_object("sw");
@@ -9,3 +12,22 @@ expander.bind_property(
   "active",
   GObject.BindingFlags.SYNC_CREATE | GObject.BindingFlags.BIDIRECTIONAL,
 );
+
+const font_button = workbench.builder.get_object("font_button");
+const font_button_label = workbench.builder.get_object("font_button_label");
+const font_dialog = new Gtk.FontChooserDialog();
+font_dialog.bind_property(
+  "font",
+  font_button_label,
+  "label",
+  GObject.BindingFlags.SYNC_CREATE | GObject.BindingFlags.BIDIRECTIONAL,
+);
+font_dialog.set_modal(true);
+font_dialog.set_transient_for(window);
+font_dialog.connect("response", () => {
+  font_dialog.close();
+});
+font_dialog.set_hide_on_close(true);
+font_button.connect("clicked", () => {
+  font_dialog.show();
+});
